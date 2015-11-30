fb0c812040c7527b77efcd243ce0e95b58ddce85
michel-tamer <michel.tamer00@gmail.com>
2015-11-30 12:32:45 -0500

    Get image and the base64 code for that image

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,22 +3,41 @@
  */
 $(document).ready(function()
 {
-
     var socket = io.connect("http://localhost:3000");
 
     socket.on('connect',function()
     {
-
-
         socket.emit("room",window.location.pathname);
-       socket.on('text',function(data){
-           $("#editor").val(data);
-       });
 
+        socket.on('text',function(data){
+
+            $("#editor").val(data);
+        });
     });
 
 
     $("#editor").on("keyup",function(){
        socket.emit("text",$(this).val());
+
     });
+
+    $(function(){
+        $('#clickme').click(function(){
+            $('#upload').click();
+        });
+    });
+    File.prototype.convert = function(callback){
+        var FR= new FileReader();
+        FR.onload = function(e) {
+            callback(e.target.result)
+        };
+        FR.readAsDataURL(this);
+    }
+    $("#upload").on('change',function(){
+        var selectedFile = this.files[0];
+        selectedFile.convert(function(base64){
+           console.log(base64);
+        })
+    });
+
 });
